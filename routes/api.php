89f352b1881b950e04d87593f3647cd03c8259b5
Traore Allassane <traoreallassane2255@gmail.com>
2025-07-19 18:06:55 +0000

    feat : Auth + Roles

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function(){
+    // Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+});
